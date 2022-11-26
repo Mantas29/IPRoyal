98b992b76401b834cc4ebd655094bcce7b73dd6f
MantasSkeiverys <mantas.skeiverys@tglab.com>
2022-11-26 12:48:11 +0200

    Created XCode project

---
+++ IPRoyal/IPRoyal/IPRoyalApp.swift
@@ -0,0 +1,17 @@
+//
+//  IPRoyalApp.swift
+//  IPRoyal
+//
+//  Created by m.skeiverys on 2022-11-26.
+//
+
+import SwiftUI
+
+@main
+struct IPRoyalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
